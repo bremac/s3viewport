2c11575e5414cca19698cc500844e7553dcc9aa8
Brendan MacDonell <brendan@macdonell.net>
2012-09-30 17:38:28 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+
+setup(name='s3viewport',
+      version='20120930',
+      description='A FUSE filesystem for viewing S3 buckets',
+      author='Brendan MacDonell',
+      author_email='brendan@macdonell.net',
+      url='https://github.com/bremac/s3viewport',
+      packages=['s3viewport'],
+      package_dir={'s3viewport': 's3viewport'},
+      scripts=['mount.s3viewport'],
+      requires=[
+          'fusepy (>=2.0.1)',
+          'boto (>=2.6.0)',
+          'iso8601 (>=0.1.4)',
+          'PyYAML (>=3.10)',
+      ],
+      provides=['s3viewport'])
